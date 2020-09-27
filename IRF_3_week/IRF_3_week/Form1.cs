12a4c64ae5d0b376fa5e9ff4eea9cdc1b4acce70
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-09-27 22:33:44 +0200

    Tetszőleges szöveg

--- IRF_3_week/IRF_3_week/Form1.cs
+++ IRF_3_week/IRF_3_week/Form1.cs
@@ -18,8 +18,8 @@ namespace IRF_3_week
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource.LastName;
-            label2.Text = Resource.FirstName;
+            label1.Text = Resource.FullName;
+            
             button1.Text = Resource.Add;
 
             listBox1.DataSource = users;
@@ -31,8 +31,7 @@ namespace IRF_3_week
         {
             var u = new User()
             {
-                LastName = textBox1.Text,
-                FirstName = textBox2.Text
+                FullName = textBox1.Text,
             };
             users.Add(u);
         }
