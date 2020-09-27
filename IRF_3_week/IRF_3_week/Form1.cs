3ad46a76e7dc93ba5ed44e387456f7acfdf41d94
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-09-27 22:30:31 +0200

    Add button action

--- IRF_3_week/IRF_3_week/Form1.cs
+++ IRF_3_week/IRF_3_week/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IRF_3_week.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,28 @@ namespace IRF_3_week
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource.LastName;
+            label2.Text = Resource.FirstName;
+            button1.Text = Resource.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }
