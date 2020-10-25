e3df6e5cfa84218633ef6c9bd3a89b9aaf6acaf1
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-10-25 19:46:22 +0100

    Commit

---
+++ week07/week07/week07/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    class DeathProbability
+    {
+        public int Age { get; set; }
+
+        public int NumberOfChildren { get; set; }
+
+        public double P { get; set; }
+    }
+}
