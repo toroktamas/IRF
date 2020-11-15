05f97c85bd24499b5d8e422518140efeeb307fb6
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-15 20:52:56 +0100

    commit

--- weeek08/week08/week08/Form1.cs
+++ weeek08/week08/week08/Form1.cs
@@ -66,7 +66,10 @@ namespace week08
 
         private void btnSelectBall_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = btnColor.BackColor
+            };
         }
 
         private void DisplayNext()
