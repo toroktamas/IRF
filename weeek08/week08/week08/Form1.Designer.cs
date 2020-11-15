a0228274661435003e55ff73ff432524549ed45f
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-15 20:47:23 +0100

    commit

--- weeek08/week08/week08/Form1.Designer.cs
+++ weeek08/week08/week08/Form1.Designer.cs
@@ -32,6 +32,9 @@
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnSelectCar = new System.Windows.Forms.Button();
+            this.btnSelectBall = new System.Windows.Forms.Button();
+            this.lblNext = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -51,15 +54,48 @@
             this.conveyorTimer.Enabled = true;
             this.conveyorTimer.Interval = 10;
             // 
+            // btnSelectCar
+            // 
+            this.btnSelectCar.Location = new System.Drawing.Point(184, 36);
+            this.btnSelectCar.Name = "btnSelectCar";
+            this.btnSelectCar.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectCar.TabIndex = 1;
+            this.btnSelectCar.Text = "CAR";
+            this.btnSelectCar.UseVisualStyleBackColor = true;
+            this.btnSelectCar.Click += new System.EventHandler(this.btnSelectCar_Click);
+            // 
+            // btnSelectBall
+            // 
+            this.btnSelectBall.Location = new System.Drawing.Point(311, 35);
+            this.btnSelectBall.Name = "btnSelectBall";
+            this.btnSelectBall.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectBall.TabIndex = 2;
+            this.btnSelectBall.Text = "BALL";
+            this.btnSelectBall.UseVisualStyleBackColor = true;
+            this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
+            // 
+            // lblNext
+            // 
+            this.lblNext.AutoSize = true;
+            this.lblNext.Location = new System.Drawing.Point(13, 35);
+            this.lblNext.Name = "lblNext";
+            this.lblNext.Size = new System.Drawing.Size(68, 13);
+            this.lblNext.TabIndex = 3;
+            this.lblNext.Text = "Coming next:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
+            this.Controls.Add(this.btnSelectCar);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +104,9 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button btnSelectCar;
+        private System.Windows.Forms.Button btnSelectBall;
+        private System.Windows.Forms.Label lblNext;
     }
 }
 
--- weeek08/week08/week08/Form1.cs
+++ weeek08/week08/week08/Form1.cs
@@ -16,17 +16,20 @@ namespace week08
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
         private IToyFactory _factory;
         public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { 
+                _factory = value;
+                DisplayNext();
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
-            Factory = new CarFactory();
 
         }
 
@@ -54,6 +57,26 @@ namespace week08
                 mainPanel.Controls.Remove(oldestBall);
                 _toys.Remove(oldestBall);
             }
+        }
+
+        private void btnSelectCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnSelectBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = lblNext.Top + lblNext.Height + 20;
+            _nextToy.Left = lblNext.Left;
+            Controls.Add(_nextToy);
         }
     }
 }
