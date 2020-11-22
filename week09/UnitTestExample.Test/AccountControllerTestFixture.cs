5de05771f8d4e0bfab9c8aac1cfc278f55ec90ca
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-22 12:01:20 +0100

    commit

--- week09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
-    class AccountControllerTestFixture
+    public class AccountControllerTestFixture
     {
+
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
