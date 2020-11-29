e2560983cb192bc8e08ddaf3845498f1704c4e1d
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-29 16:01:52 +0100

    c

--- week10/week10/week10/Form1.cs
+++ week10/week10/week10/Form1.cs
@@ -35,6 +35,11 @@ namespace week10
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
 
         private void Gc_GameOver(object sender)
