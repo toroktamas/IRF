cd1ee2eeeaa8d1922af4db26b51ae24eb09a0a15
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-29 16:01:36 +0100

    commit

--- week10/week10/week10/Form1.cs
+++ week10/week10/week10/Form1.cs
@@ -28,11 +28,21 @@ namespace week10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
