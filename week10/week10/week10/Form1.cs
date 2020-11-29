2bfa8140e15c8bc22b743234d0d73ba4d35e1ce0
DESKTOP-SCMPU9A\Tom <torok.tamas.levente@gmail.com>
2020-11-29 15:48:37 +0100

    c

--- week10/week10/week10/Form1.cs
+++ week10/week10/week10/Form1.cs
@@ -16,6 +16,11 @@ namespace week10
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,8 +28,11 @@ namespace week10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
